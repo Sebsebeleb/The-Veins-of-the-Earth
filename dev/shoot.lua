6555a55ffe44401ef1089afea26aa5fdf46464bf
Zireael07 <katarzynamosz@gmail.com>
2013-07-11 15:11:08 +0200

    another attempt at shoot

--- dev/shoot.lua
+++ dev/shoot.lua
@@ -1,24 +1,33 @@
 -- Default archery attack
-newTalent{
+newTalent{	
 	name = "Shoot",
 	type = {"special/special", 1},
-	no_energy = "fake",
-	hide = true,
-	innate = true,
+	mode = 'activated',
+	--require = ,
 	points = 1,
-	range = archery_range,
-	message = "@Source@ shoots!",
+	cooldown = 8,
+	tactical = { ATTACK = 1 },
 	requires_target = true,
-	tactical = { ATTACK = { weapon = 1 } },
-	on_pre_use = function(self, t, silent) if not self:hasArcheryWeapon() then if not silent then game.logPlayer(self, "You require a bow or sling for this talent.") end return false end return true end,
-	no_unlearn_last = true,
+
 	action = function(self, t)
-		local targets = self:archeryAcquireTargets(nil, {one_shot=true})
-		if not targets then return end
-		self:archeryShoot(targets, t, nil)
+		local weapon = self:getInven("MAIN_HAND")[1]
+		local wep_range = weapon.combat.range
+		local tg = {type="bolt", range=wep_range, talent=t}
+		local x, y = self:getTarget(tg)
+		local _ _, _, _, x, y = self:canProject(tg, x, y)
+		if not self:getInven("MAIN_HAND")  then return end
+		if not x or not y then return nil end
+		if not wep_range then 
+			game.log(("You can't shoot a melee weapon!"))
+			return nil end
+
+		local damage = weapon.combat.dam
+
+		self:projectile(tg, x, y, DamageType.PHYSICAL, damage)
 		return true
 	end,
 	info = function(self, t)
-		return ([[Shoot your bow or sling!]])
+		--local dam = damDesc(self, DamageType.ICE, t.getDamage(self, t))
+		return ([[You shoot at the target.]])
 	end,
 }
